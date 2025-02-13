42fa1a4d38f57e3ee4099a4ffee4119a03af1cc2
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-02-13 09:38:32 +0300

    Initial Commit

---
+++ InternshipTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternshipTasks
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 13.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
