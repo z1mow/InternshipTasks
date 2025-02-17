c5e86a403cef5867933a1ede4fafe3f9c8b4fb4a
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-02-17 09:37:11 +0300

    Setup tableView

--- InternshipTasks/MainViewController.swift
+++ InternshipTasks/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  InternshipTasks
 //
 //  Created by Şakir Yılmaz ÖĞÜT on 13.02.2025.
@@ -7,14 +7,42 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let appNames: [String] = ["Pharmacy"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+       
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AppCell", for: indexPath)
+        cell.textLabel?.text = appNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+        switch indexPath.row {
+        case 0:
+            let pharmacyVC = storyboard.instantiateViewController(withIdentifier: "PharmacyViewController")
+            pharmacyVC.modalPresentationStyle = .fullScreen
+            self.present(pharmacyVC, animated: true, completion: nil)
+        default:
+            break
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
---
+++ InternshipTasks/PharmacyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PharmacyViewController.swift
+//  InternshipTasks
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 13.02.2025.
+//
+
+import UIKit
+
+class PharmacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
